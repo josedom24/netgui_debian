60a6e28503533de76ff8f507a1cebf26ef6c1ffb
José Domingo Muñoz Rodríguez <josedom24@gmail.com>
2013-11-05 13:02:17 +0100

    Versión 1.0.1

--- netgui-autoinstall.sh
+++ netgui-autoinstall.sh
@@ -10,7 +10,7 @@ rm -f netgui-${NETGUI_VERSION}.tar.bz2
 wget ${URL}/netgui-${NETGUI_VERSION}.tar.bz2
 cd /usr/local
 rm -rf netkit
-pv /tmp/netgui-${NETGUI_VERSION}.tar.bz2 | sudo tar -xjSf -
+pv /tmp/netgui-${NETGUI_VERSION}.tar.bz2 | tar -xjSf -
 ln -fs /usr/local/netkit/netgui/bin/netgui.sh /usr/local/bin
 ln -fs /usr/local/netkit/netgui/bin/clean-netgui.sh /usr/local/bin
 ln -fs /usr/local/netkit/netgui/bin/clean-vm.sh /usr/local/bin
